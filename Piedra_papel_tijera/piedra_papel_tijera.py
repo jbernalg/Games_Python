d8dc8a12520ed32af65c359b741cacd4e99009b5
jbernalg <jeinffersonbernal@gmail.com>
2022-05-23 18:29:14 -0500

    juego piedra papel tijera resuelto

--- Piedra_papel_tijera/piedra_papel_tijera.py
+++ Piedra_papel_tijera/piedra_papel_tijera.py
@@ -12,3 +12,23 @@ def jugar():
     'ti' para tijera \n""").lower()
 
     computadora = random.choice(['pi', 'pa', 'ti'])
+
+    if usuario == computadora:
+        return '¡Empate!'
+
+    if gana_jugador(usuario, computadora):
+        return '¡Ganaste!'
+
+    return '¡Perdiste!'
+
+
+def gana_jugador(jugador, oponente):
+    if ((jugador == 'pi' and oponente == 'ti') or (jugador == 'ti' and oponente == 'pa') or (jugador == 'pa' and oponente == 'pi')):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(jugar())
+
