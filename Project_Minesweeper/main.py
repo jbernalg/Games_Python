a52b35ceb9856f8020aa054c3cd1b68810e1f9c1
jbernalg <jeinffersonbernal@gmail.com>
2022-06-30 18:20:14 -0500

    Minesweeper Game

---
+++ Project_Minesweeper/main.py
@@ -0,0 +1,10 @@
+
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.configure(bg='black')
+root.geometry('800x500')  #size window
+root.title('Minesweeper Game')  #title window 
+root.resizable(False, False)   #resizable window
+root.mainloop()
