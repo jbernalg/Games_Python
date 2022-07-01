d9f04496a25d9d74398e204fef6c0d88a161a98e
jbernalg <jeinffersonbernal@gmail.com>
2022-06-30 19:26:32 -0500

    frames

--- Project_Minesweeper/main.py
+++ Project_Minesweeper/main.py
@@ -1,10 +1,29 @@
 
 from tkinter import *
-from tkinter.ttk import *
 
 root = Tk()
+#override the settings of the window
 root.configure(bg='black')
-root.geometry('800x500')  #size window
+root.geometry('800x400')  #size window
 root.title('Minesweeper Game')  #title window 
 root.resizable(False, False)   #resizable window
-root.mainloop()
+
+top_frame = Frame(
+    root,
+    bg='red', #change later to black
+    width=800,
+    height=100
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='lightblue', #change later to black
+    width=200,
+    height=500
+)
+left_frame.place(x=0, y=100)
+
+
+#Run the window
+root.mainloop() 
