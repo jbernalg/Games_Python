c980dcff6cbc0a6db2c14e64e811a5f11cdb8f20
jbernalg <jeinffersonbernal@gmail.com>
2022-07-07 21:43:39 -0500

    Display game data 2

--- Project_Minesweeper/cell.py
+++ Project_Minesweeper/cell.py
@@ -1,11 +1,12 @@
 import imp
-from tkinter import Button
+from tkinter import Button, Label
 from turtle import left
 import random
 import settings
 
 class Cell:
     all = []
+    cell_count_label_object = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
@@ -25,6 +26,15 @@ class Cell:
         btn.bind('<Button-1>', self.left_click_actions) #left click
         btn.bind('<Button-3>', self.right_click_actions) #right click
         self.cell_btn_object = btn
+
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            text=f'Cells Left: {settings.CELL_COUNT}'
+        )
+        Cell.cell_count_label_object = lbl
+
 
     def left_click_actions(self, event):
         if self.is_mine:
--- Project_Minesweeper/main.py
+++ Project_Minesweeper/main.py
@@ -47,7 +47,10 @@ for x in range(settings.GRID_SIZE):
             column=x, row=y
         )
 
-Cell.randomize_mines()
+#call the label from the cell class
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(x=0, y=0)
 
+Cell.randomize_mines()
 #Run the window
 root.mainloop() 
